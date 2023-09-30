59b5f0be9f45a91453dc4521c1f5728b65c027eb
Cart1416 <83319239+Cart1416@users.noreply.github.com>
2023-09-29 21:38:41 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,39 @@
-from flask import Flask
-
+from flask import Flask,request,redirect,Response
+import requests
 app = Flask(__name__)
+SITE_NAME = "https://now.gg"
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/",methods=['GET','POST'])
+def index():
+    global SITE_NAME
+    if request.method=="GET":
+        resp = requests.get(f"{SITE_NAME}")
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    elif request.method=="POST":
+        resp = requests.post(f"{SITE_NAME}",data=request.form)
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/<path:path>",methods=["GET","POST"])
+def proxy(path):
+    global SITE_NAME
+    if request.method=="GET":
+        resp = requests.get(f"{SITE_NAME}{path}")
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    elif request.method=="POST":
+        resp = requests.post(f"{SITE_NAME}{path}",data=request.form)
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=1337)
